940fb1caa481978d90e63ddab746f2232dff689e
jprosario <jomi0388@gmail.com>
2016-02-06 10:42:11 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |attribute, type|
+    type.each do |char, names|
+      names.each do |name|
+        new_hash[name] ||= {}
+        new_hash[name][attribute] ||= []
+        new_hash[name][attribute] << char.to_s
+      end
+    end
+  end
+  new_hash
+  #binding.pry
 end
